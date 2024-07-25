f54de4c7111cedf415e8171febacb390b8afe943
Florian Schneider <schneider.f@me.com>
2024-07-25 13:35:03 +0200

    strapi on render

--- src/api/landingpage-image/models/landingpageimage.js
+++ src/api/landingpage-image/models/landingpageimage.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_API_URL;
@@ -9,24 +11,23 @@ module.exports = {
     async afterCreate(event) {
       const { result } = event;
 
-      if (result.image && result.image.url) {
-        const { url } = result.image;
+      if (result.Bilder && result.Bilder.url) {
+        const { url } = result.Bilder;
         const { error } = await supabase
           .from("landingpage_images")
-          .insert({ image_url: url, id: result.id });
+          .update({ image_url: url })
+          .eq("id", result.id);
 
         if (error) {
           console.error("Error inserting image URL into Supabase:", error);
-        } else {
-          console.log("Successfully inserted image URL into Supabase.");
         }
       }
     },
     async afterUpdate(event) {
       const { result } = event;
 
-      if (result.image && result.image.url) {
-        const { url } = result.image;
+      if (result.Bilder && result.Bilder.url) {
+        const { url } = result.Bilder;
         const { error } = await supabase
           .from("landingpage_images")
           .update({ image_url: url })
@@ -34,8 +35,6 @@ module.exports = {
 
         if (error) {
           console.error("Error updating image URL in Supabase:", error);
-        } else {
-          console.log("Successfully updated image URL in Supabase.");
         }
       }
     },
